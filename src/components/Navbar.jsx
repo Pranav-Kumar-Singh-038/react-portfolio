d63f602939576f5d57144d45f6d7fde71d926a36
Curious-Goblin <poddarsourabh9939@gmail.com>
2024-04-24 21:53:29 +0530

    adjusted hamberger menu in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export function Navbar() {
                 <div>DUMMY</div>
                 <div className="text-red-800">NAME</div>
             </div>
-            <div className="hidden md:block md:flex md:gap-8 md:items-center">
+            <div className="hidden md:block md:flex gap-8 items-center">
                 <a href="#"><div className="text-red-800 hover:text-red-600">Home</div></a>
                 <a href="#about"><div className="hover:text-slate-500">About</div></a>
                 <Popup trigger={
@@ -38,19 +38,17 @@ export function Navbar() {
                         <div>Book Us</div>
                     </button>
                 </a>
-                <div className=''>
+                <div>
                     <Popup
                         trigger={
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                                </svg>
-                            </div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            </svg>
                         }
-                        position="bottom right" on="hover" closeOnDocumentClick mouseLeaveDelay={300} mouseEnterDelay={300} contentStyle={{ padding: '0px', border: 'none', width: '100px' }} arrow={false}>
-                        <div className=" flex-col justify-center ">
+                        position="bottom right" on="click" closeOnDocumentClick mouseLeaveDelay={300} mouseEnterDelay={300} contentStyle={{ padding: '0px', border: 'none', width: '100px' }} arrow={false}>
+                        <div className="flex-col justify-center ">
                             <div className="flex justify-left hover:bg-gray-500 hover:text-white px-4 py-2 transition-colors duration-300"> <a href='#'>Home</a></div>
-                            <div className="flex justify-left  hover:bg-gray-500 hover:text-white px-4 py-2 transition-colors duration-300"> <a href='#About'>About</a></div>
+                            <div className="flex justify-left  hover:bg-gray-500 hover:text-white px-4 py-2 transition-colors duration-300"> <a href='#about'>About</a></div>
                             <div className="flex justify-left  hover:bg-gray-500 hover:text-white px-4 py-2 transition-colors duration-300"> <a href='#gallery'>Media</a></div>
                             <div className="flex justify-left  hover:bg-gray-500 hover:text-white px-2 py-2 transition-colors duration-300"> <a href='#services'>Consultancy</a></div>
                             <div className="flex justify-left  hover:bg-gray-500 hover:text-white px-4 py-2 transition-colors duration-300"> <a href='#buttonbar'>Coverage</a></div>
@@ -64,3 +62,5 @@ export function Navbar() {
         </div>
     )
 }
+
+
